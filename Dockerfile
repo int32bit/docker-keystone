58041681081e4061de8e70f5a806f9faafa71718
fgp <krystism@gmail.com>
2015-01-17 08:09:34 +0000

    rebuild

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,26 @@
-FROM krystism/openstack_base
+FROM ubuntu:14.04
 MAINTAINER krystism "krystism@gmail.com"
 # install packages
-RUN apt-get -y install keystone
-
-# remove the SQLite database file
-RUN rm -f /var/lib/keystone/keystone.db
-
+RUN set -x \
+	&& echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/juno main" > /etc/apt/sources.list.d/juno.list \
+	&& apt-get -y update \
+	&& apt-get -y install ubuntu-cloud-keyring \
+	&& apt-get -y update \
+	&& apt-get -y install \
+		mysql-client \
+		keystone \
+		python-keystoneclient \
+		python-mysqldb \
+	&& apt-get -y clean \
+	&& rm -f /var/lib/keystone/keystone.db
+VOLUME /etc/keystone
 EXPOSE 5000 35357
-
 # copy sql script
 COPY keystone.sql /root/keystone.sql
-
 # copy keystone config file
 COPY keystone.conf /etc/keystone/keystone.conf
 
-
 # add bootstrap script and make it executable
 COPY bootstrap.sh /etc/bootstrap.sh
-RUN chown root.root /etc/bootstrap.sh
-RUN chmod 744 /etc/bootstrap.sh
-
+RUN chown root.root /etc/bootstrap.sh && chmod a+x /etc/bootstrap.sh
 ENTRYPOINT ["/etc/bootstrap.sh"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-
+set -x
 # create database for keystone
 export MYSQL_ROOT_PASSWORD=${MYSQL_ENV_MYSQL_ROOT_PASSWORD}
-export MYSQL_HOST=mysql
+export MYSQL_HOST=${MYSQL_HOST:-mysql}
 SQL_SCRIPT=/root/keystone.sql
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST <$SQL_SCRIPT
 
 
+# init the arguments
+ADMIN_TOKEN=${ADMIN_TOKEN:-ADMIN_TOKEN}
+ADMIN_TENANT_NAME=${OS_TENANT_NAME:-admin}
+ADMIN_USER_NAME=${OS_USERNAME:-admin}
+ADMIN_PASSWORD=${OS_PASSWORD:-ADMIN_PASS}
+ADMIN_EMAIL=${OS_ADMIN_EMAIL:-${ADMIN_USER_NAME}@example.com}
+OS_SERVICE_TOKEN=$ADMIN_TOKEN
+OS_SERVICE_ENDPOINT=${OS_AUTH_URL:-"http://${HOSTNAME}:35357/v2.0"}
+unset OS_TENANT_NAME OS_USERNAME OS_PASSWORD OS_AUTH_URL
+
 # modify the config file
 CONFIG_FILE=/etc/keystone/keystone.conf
 sed -i "s#^connection.*=.*#connection = mysql://keystone:KEYSTONE_DBPASS@${MYSQL_HOST}/keystone#" $CONFIG_FILE
-export ADMIN_TOKEN=${ADMIN_TOKEN:-ADMIN_TOKEN}
+ADMIN_TOKEN=${ADMIN_TOKEN:-ADMIN_TOKEN}
 sed -i "s#^admin_token.*=.*#admin_token = $ADMIN_TOKEN#" $CONFIG_FILE
 su -s /bin/sh -c "keystone-manage db_sync" keystone
 
@@ -19,13 +29,8 @@ keystone-all &
 # it may take some time to start keystone service, waiting for it.
 sleep 5
 
-export OS_SERVICE_TOKEN=${ADMIN_TOKEN}
-export OS_SERVICE_ENDPOINT=http://${HOSTNAME}:35357/v2.0
-
-ADMIN_TENANT_NAME=${ADMIN_TENANT_NAME:-admin}
-ADMIN_USER_NAME=${ADMIN_USER_NAME:-admin}
-ADMIN_PASSWORD=${ADMIN_PASSWORD:-ADMIN_PASS}
-ADMIN_EMAIL=${ADMIN_EMAIL:-admin@example.com}
+# keystone init
+export OS_SERVICE_TOKEN OS_SERVICE_ENDPOINT
 keystone tenant-create --name $ADMIN_TENANT_NAME --description "Admin Tenant"
 keystone user-create --name $ADMIN_USER_NAME --pass $ADMIN_PASSWORD --email $ADMIN_EMAIL
 keystone role-create --name admin
@@ -34,22 +39,16 @@ keystone role-create --name _member_
 keystone user-role-add --tenant $ADMIN_TENANT_NAME --user $ADMIN_USER_NAME --role _member_
 keystone tenant-create --name service --description "Service Tenant"
 keystone service-create --name keystone --type identity --description "OpenStack Identity"
-KEYSTONE_HOST=${KEYSTONE_HOST:-$HOSTNAME}
+KEYSTONE_HOST=$HOSTNAME
 keystone endpoint-create \
 	--service-id $(keystone service-list | awk '/ identity / {print $2}') \
 	--publicurl http://${KEYSTONE_HOST}:5000/v2.0 \
 	--internalurl http://${KEYSTONE_HOST}:5000/v2.0 \
 	--adminurl http://${KEYSTONE_HOST}:35357/v2.0 \
 	--region regionOne
-# create a admin-openrc.sh file
-ADMIN_OPENRC=/root/admin-openrc.sh
-cat >$ADMIN_OPENRC <<EOF
-export OS_TENANT_NAME=$ADMIN_TENANT_NAME
-export OS_USERNAME=$ADMIN_USER_NAME
-export OS_PASSWORD=$ADMIN_PASSWORD
-export OS_AUTH_URL=$OS_SERVICE_ENDPOINT
-EOF
 unset OS_SERVICE_TOKEN OS_SERVICE_ENDPOINT
 # FIXME I need restart
 pkill keystone-all
+set +x
+echo "OK"
 keystone-all
